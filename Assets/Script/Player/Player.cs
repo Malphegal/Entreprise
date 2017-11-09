7eeac368ddd38bcfc7127968af679c1a0ad07210
Sarah Matter <s.matter@unistra.fr>
2017-11-09 18:09:38 +0100

    Ajout du dossier Audio dans les Assets

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,16 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Linq;
 
-public class Player : MonoBehaviour {
+public class Player
+{
+        // FIELDS
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // PROPERTIES
+
+    public string Name      { get; private set; }   // Le nom du joueur
+    public int HP           { get; private set; }   // La quantité de point de vie du joueur
+    public int AttackValue  { get; private set; }   // La valeur d'attaque de base (sans prendre en compte l'équipement) du personnage
+    public int DefenceValue { get; private set; }   // La valeur de défense de base (sans prendre en compte l'équipement) du personnage.
+    public int SpeedValue   { get; private set; }   // La valeur de la vitesse de déplacement de base du personnage, sans prendre en compte le poids de l'équipement
+
+
+        // CONSTRUCTORS
+
+    public Player(string name)
+    {
+        this.Name = name;
+    }
+
+        // METHODS
+
+        // STATIC METHODS
+
 }
