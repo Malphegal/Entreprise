9fadfb6496edf154ae84d0f0bd111af0442535ed
Malphegal <nateag_96@hotmail.fr>
2017-11-09 21:39:13 +0100

    Ajout de la classe 'MainMenu' et de la scène 'Base world'

---
+++ Assets/Script/Menu/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    // TODO: Mettre sous un Thread pour avoir une barre de chargement
+    public void OnClickNewGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
