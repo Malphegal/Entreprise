2fab6f9124c825246537efa15311cf061fe4ba8d
Malphegal <nateag_96@hotmail.fr>
2017-11-13 09:47:24 +0100

    (1)

--- L'IA/Projet_AIA_Console_Version/Projet_AIA_Console_Version/Natures_Grammaticales/Divers/RemplissageAuto.cs
+++ L'IA/Projet_AIA_Console_Version/Projet_AIA_Console_Version/Natures_Grammaticales/Divers/RemplissageAuto.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Projet_AIA_Console_Version.Natures_Grammaticales.Divers
 {
-    class RemplissageAuto
+    public static class RemplissageAuto
     {
+        static void Init()
+        {
+
+        }
     }
 }
