c87a8e741897c2e18e03bdefb415d6460fe22876
Sarah Matter <s.matter@unistra.fr>
2017-11-13 09:20:11 +0100

    Ajout d'une classe RemplissageAuto.

---
+++ L'IA/Projet_AIA_Console_Version/Projet_AIA_Console_Version/Natures_Grammaticales/Divers/RemplissageAuto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_AIA_Console_Version.Natures_Grammaticales.Divers
+{
+    class RemplissageAuto
+    {
+    }
+}
