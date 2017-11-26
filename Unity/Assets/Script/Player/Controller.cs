bc24e6ff81412ad97887f0ba1a50129ae6d609ec
Malphegal <nateag_96@hotmail.fr>
2017-11-26 11:19:06 +0100

    Unity : Ajout de la classe Controller

---
+++ Unity/Assets/Script/Player/Controller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Classe permettant le déplacement du modèle du joueur.
+/// </summary>
+public class Controller : MonoBehaviour {
+
+        // FIELDS
+
+    Rigidbody _rb;
+    byte _speed;
+    bool _isJumping;
+    bool _canJump;
+    float _jumpPower;
+
+        // METHODS
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _speed = 10;
+        _isJumping = false;
+        _canJump = true;
+        _jumpPower = 850;
+
+        print("I'm awake");
+    }
+
+    void Start () {
+        print("I'm started");
+	}
+	
+	void Update () {
+        if (Input.GetButtonDown("Jump") && !_isJumping && _canJump)
+        {
+            _isJumping = true;
+            _canJump = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _rb.MovePosition(_rb.position + transform.TransformDirection(direction) * _speed * Time.deltaTime);
+
+        if (_isJumping)
+        {
+            _rb.AddForce(transform.up * _jumpPower);
+            _isJumping = false;
+        }
+        _rb.AddForce(Physics.gravity * GetComponent<Rigidbody>().mass);
+    }
+
+    /* TODO: Vérifier si la collision se fait avec un tag de terrain */
+    private void OnCollisionEnter(Collision collision)
+    {
+        _canJump = true;
+    }
+}
---
+++ Unity/Assets/Script/Player/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// La classe représentant les statistiques du joueur.
+/// </summary>
+public class Player
+{
+        // PROPERTIES
+
+    public string   Pseudo      { get; private set; }   // Le pseudo du joueur
+
+    public ushort   Hunger      { get; private set; }   // Le niveau de faim du joueur
+    public ushort   Thrist      { get; private set; }   // Le niveau de soif du joueur
+
+    public byte     Speed       { get; private set; }   // La vitesse du joueur
+
+    public int      CurrentHP   { get; private set; }   // Les points de vie acutels du joueur
+    public int      MaxHP       { get; private set; }   // Le nombre de points de vie maximum du joueur
+
+        // METHODS
+    
+    public override string ToString()
+    {
+        return "[" + this.Pseudo + "] " + this.CurrentHP + " / " + this.MaxHP + " PV  -  ";
+    }
+
+    public void Eat()
+    {
+
+    }
+}
